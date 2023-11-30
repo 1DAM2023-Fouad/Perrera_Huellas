feee2c299253ddfded4728b1555208bbd0d61d1d
Fouad <fouadaharchi1@gmail.com>
2023-11-30 01:56:08 +0100

    Creado clases con dependecias, captura diagrama uml, readme y el main del ejercicio

---
+++ src/Adopcion.java
@@ -0,0 +1,47 @@
+public class Adopcion {
+    private String codigo;
+    private String fechaAdopcion;
+    private Persona persona;
+    private Gato gato;
+    private Perro perro;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getFechaAdopcion() {
+        return fechaAdopcion;
+    }
+
+    public void setFechaAdopcion(String fechaAdopcion) {
+        this.fechaAdopcion = fechaAdopcion;
+    }
+
+    public Persona getPersona() {
+        return persona;
+    }
+
+    public void setPersona(Persona persona) {
+        this.persona = persona;
+    }
+
+    public Gato getGato() {
+        return gato;
+    }
+
+    public void setGato(Gato gato) {
+        this.gato = gato;
+    }
+
+    public Perro getPerro() {
+        return perro;
+    }
+
+    public void setPerro(Perro perro) {
+        this.perro = perro;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
 public class Main {
     public static void main(String[] args) {
 
+        // Crear nueva adopcion
+        Adopcion adopcion = new Adopcion();
+        adopcion.setCodigo("#654910");
+        adopcion.setFechaAdopcion("30/11/2023");
+
+
+        // Crear nueva persona
+        Persona persona = new Persona();
+        persona.setNombre("Rubén");
+        persona.setApellidos("García");
+        persona.setDni("41269406-R");
+        persona.setTelefono("688269467");
+        persona.setAdopcion(persona.getAdopcion());
+
+        // Crear nuevo Animal: Perro
+        Perro perro = new Perro();
+        perro.setCoidoUnico("#654654");
+        perro.setNombre("Rex");
+        perro.setFechaNacimiento("02/06/2019");
+        perro.setAmigable("Es amigable, se lleva bien con otros perros :)");
+        perro.setAdopcion(perro.getAdopcion());
+
+        // Crear nuevo Animal: Gato
+        Gato gato = new Gato();
+        gato.setCoidoUnico("#548465");
+        gato.setNombre("Lope");
+        gato.setFechaNacimiento("15/10/2021");
+        gato.setVirusLeucemia("Tiene virus de Leucemia :(");
+        gato.setAdopcion(gato.getAdopcion());
+
+
+        // Le pasamos el objeto persona, perro, gato al objeto adopcion,
+        //para que pueda mostarar todos sus atributos
+        adopcion.setPersona(persona);
+        adopcion.setPerro(perro);
+        adopcion.setGato(gato);
+
+
+        //Imrprimir objeto adopcion
+        System.out.println(adopcion);
     }
+
 }
